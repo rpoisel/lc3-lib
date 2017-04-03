e084b65acded8c1bd704f38e7613fa3cfabd47fc
Rainer Poisel <rainer.poisel@gmail.com>
2017-04-03 23:30:39 +0200

    I2HAA and I2HAE code refactoring and variants

--- I2C/lcfu___i2c_smbus_read_block_data.c
+++ I2C/lcfu___i2c_smbus_read_block_data.c
@@ -11,12 +11,14 @@
 #include <lcfu___i2c_smbus_read_block_data.h>
 
 /*                            Functions                        */
-void  lcfu___I2C_SMBUS_READ_BLOCK_DATA(LC_TD_Function_I2C_SMBUS_READ_BLOCK_DATA* LC_this, LC_TD_DINT LC_VD_FD, LC_TD_USINT LC_VD_COMMAND, struct _lcoplck_epdb_1_impl* pEPDB)
+void  lcfu___I2C_SMBUS_READ_BLOCK_DATA(LC_TD_Function_I2C_SMBUS_READ_BLOCK_DATA* LC_this, LC_TD_DINT LC_VD_FD, LC_TD_USINT LC_VD_COMMAND, LC_TD_USINT LC_VD_LEN, struct _lcoplck_epdb_1_impl* pEPDB)
 {
-	LC_this->LC_VD_I2C_SMBUS_READ_BLOCK_DATA = i2c_smbus_read_block_data(LC_VD_FD, LC_VD_COMMAND, LC_this->LC_VD_VALUES);
-	#if DEBUG==1
-		printf("i2c_smbus_read_block_data(%d, 0x%02X)\n", LC_VD_FD, LC_VD_COMMAND);
-	#endif
+	LC_this->LC_VD_I2C_SMBUS_READ_BLOCK_DATA =
+			i2c_smbus_read_i2c_block_data(
+					LC_VD_FD, LC_VD_COMMAND, LC_VD_LEN > sizeof(LC_this->LC_VD_VALUES) ? sizeof(LC_this->LC_VD_VALUES) : LC_VD_LEN, LC_this->LC_VD_VALUES);
+#if DEBUG==1
+	printf("i2c_smbus_read_block_data(%d, 0x%02X)\n", LC_VD_FD, LC_VD_COMMAND);
+#endif
 }
 
 #endif
--- I2C/lcfu___i2c_smbus_write_block_data.c
+++ I2C/lcfu___i2c_smbus_write_block_data.c
@@ -13,10 +13,10 @@
 /*                            Functions                        */
 void  lcfu___I2C_SMBUS_WRITE_BLOCK_DATA(LC_TD_Function_I2C_SMBUS_WRITE_BLOCK_DATA* LC_this, LC_TD_DINT LC_VD_FD, LC_TD_USINT LC_VD_COMMAND, LC_TD_USINT LC_VD_LEN, LC_TD_USINT LC_VD_VALUES[34], struct _lcoplck_epdb_1_impl* pEPDB)
 {
-	LC_this->LC_VD_I2C_SMBUS_WRITE_BLOCK_DATA = i2c_smbus_write_block_data(LC_VD_FD, LC_VD_COMMAND, LC_VD_LEN, LC_VD_VALUES);
-	#if DEBUG==1
-		printf("i2c_smbus_write_block_data(%d, 0x%02X)\n", LC_VD_FD, LC_VD_COMMAND);
-	#endif
+	LC_this->LC_VD_I2C_SMBUS_WRITE_BLOCK_DATA = i2c_smbus_write_i2c_block_data(LC_VD_FD, LC_VD_COMMAND, LC_VD_LEN, LC_VD_VALUES);
+#if DEBUG==1
+	printf("i2c_smbus_write_block_data(%d, 0x%02X)\n", LC_VD_FD, LC_VD_COMMAND);
+#endif
 }
 
 #endif
